5b41d468674a499322626451856dff4ecab4e6e0
SuhadMendis <suhad.a.mendis@gmail.com>
2021-09-19 15:42:52 +0530

    up

--- portal.php
+++ portal.php
@@ -25,7 +25,9 @@
 
 <h2>Registration</h2>
 
-<table id="example" class="display" style="width:100%">
+
+
+<!-- <table id="example" class="display" style="width:100%">
         <thead>
             <tr>
                 <th>Name</th>
@@ -58,18 +60,21 @@
                 <th>Mobile</th>
             </tr>
         </tfoot>
-    </table>
-
-
+    </table> -->
   
 
 
 </div>
 
 <script>
+
 $(document).ready(function(){
+    
     $("#pallet").hide();
+
 });
+
+document.getElementById('pallet').innerHTML = '<table id="example" class="display" style="width:100%"><thead><tr><th>Name</th><th>Mobile</th><th>Email</th><th>Speciality</th></tr></thead> <tfoot>   <tr>       <th>Name</th>       <th>Mobile</th>       <th>Email</th>       <th>Speciality</th>   </tr> </tfoot>  </table<hr> <h2>Login</h2> <table id="example1" class="display" style="width:100%">   <thead>   <tr>   <th>Name</th>    <th>Mobile</th>  </tr>  </thead>   <tfoot>    <tr>     <th>Name</th>       <th>Mobile</th>      </tr>    </tfoot>  </table>';
 
 </script>
 
