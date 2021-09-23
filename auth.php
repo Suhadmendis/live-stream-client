d0517f5666892efbc9743e67f75a4a82b6669db1
Akila Mendis <akila@stacktech.io>
2021-09-23 22:25:00 +0530

    up

--- auth.php
+++ auth.php
@@ -7,6 +7,10 @@
     <title>Tallees - Live Stream</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="_css/login_style.css?v=0000000001">
     <script src="js/user.js?v=0000000001"></script>
 
@@ -85,9 +89,9 @@
 
                 <p id="person-name">Dr Laura Savage</p>
                 <p id="person-position">MBChB, BSc (Hons), MRCP (UK), MRCP (Dermatology), PhD</p>
-                <p id="person-des">Consultant Dermatologist, Leeds Centre for Dermatology,
+                <p id="person-des">Consultant Dermatologist, Leeds Centre for Dermatology,  <br>
 
-                    Chapel Allerton Hospital and Honorary Senior Lecturer,
+                    Chapel Allerton Hospital and Honorary Senior Lecturer, <br>
 
                     University of Leeds, UK</p>     
 
