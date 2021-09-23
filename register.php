fe2646ef1b8760b645413cbab20715f26c98bbc2
SuhadMendis <suhad.a.mendis@gmail.com>
2021-09-23 11:31:10 +0530

    up

--- register.php
+++ register.php
@@ -59,7 +59,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="offset-md-10 col-md-10">
                 <img src="_img/novartis/registration/Registration-Screen-1920-x-1080-Top.png" alt="">
             </div>
         </div>
